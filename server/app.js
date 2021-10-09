66c60e3e055060fa88ba788567b00ba9239313d6
Manuel Moran <manuel9423@hotmail.com>
2021-10-09 13:25:24 +0200

    2 folders

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+require("dotenv").config();
+
+
+const app = express()
+const port = process.env.PORT
+
+// Para evitar problemas de CORS (Cross-Origin Resource Sharing)
+// Añado un middleWare que permite consultas desde orígines de terceros
+// ...
+
+
+// Para habilitar recepcion de JSONs
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//API
+
+//app.use('/api',routerApi); // rutas para API
+
+// WEB: usar el Router
+//app.use('/',router); // rutas para WEB
+
+
+// Manejo de errores
+
+
+// Para manejar cualquier otro tipo de error de mi servidor:
+app.get('*', (req, res)=>{
+    res.status(404).send("Sorry... 404 Not Found");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
